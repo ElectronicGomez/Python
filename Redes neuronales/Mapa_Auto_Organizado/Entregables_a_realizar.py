6f7be9cfdf435e9603a0811bae10428a29deccb3
Maikol Sánchez <89115992+ElectronicGomez@users.noreply.github.com>
2021-08-18 23:21:01 -0500

    Add files via upload

---
+++ Redes neuronales/Mapa_Auto_Organizado/Entregables_a_realizar.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 27 09:43:02 2020
+
+@author: ASUS
+"""
+
+#%%
+'''Vamos a combinar dos redes neuronales, la red neuronal artificial que se hará para
+saber la probabilidad de que sean estafados por pishing y el mapa auto organizado que
+se hizo anteriormente'''
+'''
+Vamos hacer un modelo mas avanzado que pueda predecir la probabilidad de que un cliente
+sea victima de fraude
+'''
+'''
+Para ello debemos pasar de deep learning no supervisado a deep learning supervisado
+'''
---
+++ Redes neuronales/Mapa_Auto_Organizado/mapa_organizado_deep_learn.py
@@ -0,0 +1,57 @@
+# Mapa Auto Organizado
+
+#%%
+# Importando Librerias
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#%%
+# Importando Sets de Datos
+dataset = pd.read_csv('BASE_DATOS.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+#%%
+# Escalado de Categorias
+from sklearn.preprocessing import MinMaxScaler
+sc = MinMaxScaler(feature_range = (0, 1))
+X = sc.fit_transform(X)
+
+#%%
+# Entrenando MAO
+from minisom import MiniSom
+som = MiniSom(x = 10, y = 10, input_len = 15, sigma = 1.0, learning_rate = 0.5)
+som.random_weights_init(X)
+som.train_random(data = X, num_iteration = 100)
+
+#%%
+# Visualizando Resultados
+from pylab import bone, pcolor, colorbar, plot, show
+bone()
+pcolor(som.distance_map().T)
+colorbar()
+markers = ['o', 's']
+colors = ['r', 'g']
+for i, x in enumerate(X):
+    w = som.winner(x)
+    plot(w[0] + 0.5,
+         w[1] + 0.5,
+         markers[y[i]],
+         markeredgecolor = colors[y[i]],
+         markerfacecolor = 'None',
+         markersize = 10,
+         markeredgewidth = 2)
+show()
+
+#%%
+# Encontrando Fraude
+mappings = som.win_map(X)
+frauds = np.concatenate((mappings[(6,3)], mappings[(7,5)]), axis = 0)
+frauds = sc.inverse_transform(frauds)
+
+
+
+
+
+
